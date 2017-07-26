d422d75882d77a082a8eb2e78f65567a1808dba8
Matthew Eldredge <mattheweldredge@gmail.com>
2017-07-26 16:35:55 -0700

    remove uneccessary comment and alert

---
+++ src/js/app.js
@@ -0,0 +1,10 @@
+//hide/show function for toggle button and li's that have been selected as done
+$(function() {
+	$('#hideShow').on('click', function(){
+		$('li.strike-through').toggleClass('hidden');
+		alert('hi');
+	})
+});
+
+
+// this is a comment
